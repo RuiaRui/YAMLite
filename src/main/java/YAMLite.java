85b09801e41441d3be2fc893328af160e175afbe
AlexR <zhuyurui0323@sina.com>
2018-06-14 12:16:21 +0800

    完善 读取文件

--- src/main/java/YAMLite.java
+++ src/main/java/YAMLite.java
@@ -1,12 +1,11 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
 
 public class YAMLite {
 
 
 
-    enum options{prase,json,find};
+    enum options{prase,json,find}
 
 
     public YAMLite(String file){
@@ -19,24 +18,26 @@ public class YAMLite {
         if(args.length==1){
             filepath=args[0];
         }else if(args.length==2){
-            if(args[0]=="-json"){
+            if(args[0].equals("-json")){
                 op=options.json;
-            } else if(args[0]=="-find"){
-                op=options.find;
+            } else if(args[0].equals("-find")) {
+                op = options.find;
+            } else {
+                op=options.prase;
             }
-            filepath=args[0];
+            filepath=args[1];
         }else {
-            System.out.println("Usage: json [-pretty][filepath][query]");
+            System.out.println("Usage: yamlite [option [value]] file");
             System.exit(0);
         }
 
         try{
             File file=new File(filepath);
             if(!file.isFile() || !file.exists()) {
-                System.out.println("No file exists!");
+                System.out.println("No such file exists, please check and try again");
             }else {
                 //具体实现
-                
+
             }
         }catch (Exception e){
             e.printStackTrace();
